8e4e5941e0a6c0206361b378443e0ecefa8ae34f
Jayanti Mahato <jayantimahato510@gmail.com>
2025-02-16 12:20:35 +0530

    3rd commit

--- backend/controllers/checkOut.js
+++ backend/controllers/checkOut.js
@@ -5,4 +5,13 @@ import {Volunteer} from "../models/volunteer.js"
 
 // base url
 
-// const cryptomus = axios.create(baseURL:"")
+const cryptomus = axios.create({baseURL:""})
+
+// const createInvoice = async(amount) =>{
+// try{
+//   const data = {
+//     amount :amount,
+//     currency
+//   }
+// }
+// }
--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -1,55 +1,77 @@
-import React ,{useState} from 'react';
+import React, { useState } from "react";
 
-
-
+import axios from "axios";
+import { toast } from "react-toastify";
 const Contact = () => {
-
-  const [name,setName] = useState("");
-  const [email,setEmail] =useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
-  const [message,setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleSendMessage = async(e) =>{
+  const handleSendMessage = async (e) => {
     e.preventDefault();
-    // await axios.post("http://localhost:4000/api/v1/message/send",{name,email,phone,message},{
-    //   withCredentials :true,
-    //   headers:{"Content-Type":"application/json"},
-    // })
-    // .then
-  }
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/message/send",
+        { name, email, phone, message },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        setName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+        toast.success(res.data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error.response.data.message);
+      });
+  };
 
   return (
-    <section className='contact'>
+    <section className="contact">
       <div className="container">
         <div className="banner">
-          <form onSubmit ={handleSendMessage}>
+          <form onSubmit={handleSendMessage}>
             <h2>Contact Us</h2>
             <div className="">
+              <input
+                type="text"
+                value={name}
+                placeholder="Your Nice Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                value={email}
+                placeholder="Your Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-              <input type='text' value={name} placeholder='Your Nice Name'  onChange={(e)=>setName(e.target.value)}/>
-              <input type="email" value={email} placeholder='Your Email' onChange={(e)=>setEmail(e.target.value)}   />
-
-              <input  type="number"
-              value={phone}
-              placeholder='Phone Number'
-              onChange={(e)=> setPhone(e.target.value)}
-               
+              <input
+                type="number"
+                value={phone}
+                placeholder="Phone Number"
+                onChange={(e) => setPhone(e.target.value)}
               />
             </div>
-            <textarea 
-            
-            rows="10"
-            value ={message}
-            placeholder='Message'
-            onChange={(e)=>setMessage(e.target.value)}
-            
+            <textarea
+              rows="10"
+              value={message}
+              placeholder="Message"
+              onChange={(e) => setMessage(e.target.value)}
             />
 
-            <button className='btn' type='sumit'>Send Message</button>
+            <button className="btn" type="sumit">
+              Send Message
+            </button>
           </form>
         </div>
       </div>
-
     </section>
   );
 };
